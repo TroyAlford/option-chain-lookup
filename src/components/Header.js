925aa2868dab04008b5d4fc11c66fda7d5483605
Troy Alford <talford@vestmark.com>
2017-12-26 12:02:01 -0800

    Header component: renders company, ticker, last price, and last update

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import toCurrency from '../utilities/toCurrency';
+
+import './Header.scss';
+
+export default ({ quote }) => (
+  <header className="option-chain">
+      <div className="security">{quote.shortName} ({quote.symbol})</div>
+      <div className="last-update">
+          <strong>Last Price:</strong> {toCurrency(quote.ask)}
+          <strong>Updated:</strong> {new Date(quote.regularMarketTime * 1000).toLocaleTimeString()}
+      </div>
+  </header>
+)
