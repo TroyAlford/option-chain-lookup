4a787da7008b3dd079748dffc3c05b594f9dc516
Troy Alford <talford@vestmark.com>
2018-01-02 08:42:44 -0800

    Adding Access-Control-Allow-Origin headers, and fixing ENOENT error

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ import express from 'express'
 import fs from 'fs-extra'
 import path from 'path'
 
+const AllowAllOrigins = (request, response, next) => {
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+};
+
 const searchAPI = express()
 	.get('/:ticker', (request, response) => {
 		const {ticker} = request.params,
@@ -13,7 +19,7 @@ const searchAPI = express()
 		response.setHeader('Content-Type', 'application/json');
 
 		if (!fs.pathExistsSync(filePath))
-			response.status(404).send(JSON.stringify({}));
+			return response.status(404).send(JSON.stringify({}));
 
 		const jsonContents = fs.readJsonSync(filePath);
 		response.status(200).send(JSON.stringify(jsonContents));
@@ -24,6 +30,7 @@ const PORT = 8081;
 
 express()
 	.use(cors())
+	.use(AllowAllOrigins)
 	.use('/api/search', searchAPI)
 	.listen(8081, () => {
 		console.log(`STARTUP: Express server running on port ${PORT}`)
